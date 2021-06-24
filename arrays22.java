a2722b8cd3b03a8bb1c4197a4ef460bd65b11c48
Shre-171 <85492752+Shre-171@users.noreply.github.com>
2021-06-24 16:48:25 +0530

    ARRAY MATRIX ADDITION
    The above file contains matrix addition with 3 rows and 3 columms

---
+++ arrays22.java
@@ -0,0 +1,22 @@
+
+package javaapplication4;
+
+public class arrays22 {
+    public static void main(String[] args){
+  int a[][]={{1,4,5},{2,7,7},{1,1,5}};
+  int b[][]={{1,4,4},{8,4,3},{1,8,2}};
+  int c[][]=new int[3][3];
+  
+  for(int i=0;i<3;i++){
+  for(int j=0;j<3;j++){
+      c[i][j]=a[i][j]+b[i][j];
+      System.out.print(c[i][j]+" ");
+  }
+  System.out.println();
+      
+  }    
+  }
+  
+    
+}
+
