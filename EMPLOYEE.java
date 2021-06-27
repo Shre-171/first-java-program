2584b547ea54203a23ecb9f1c0638a450d8c4a4f
Shre-171 <85492752+Shre-171@users.noreply.github.com>
2021-06-27 13:32:57 +0530

    EMPLOYEE DETAILS
    the file takes inputs of 3 employees

---
+++ EMPLOYEE.java
@@ -0,0 +1,75 @@
+
+package javaapplication4;
+
+import java.util.Scanner;
+
+
+public class EMPLOYEE {
+   public static void main(String[] args){
+       
+    
+    String name,address;
+    int yearjoined;
+    float salary;
+    
+    Scanner sc=new Scanner(System.in);
+    System.out.println("ENTER NAME OF EMPLOYEE1: ");
+    name=sc.nextLine();
+    
+    System.out.println("ENTER NAME OF EMPLOYEE'S ADDRESS : ");
+    address=sc.nextLine();
+    
+    System.out.println("ENTER THE YEAR OF JOINING : ");
+    yearjoined=sc.nextInt();
+    
+    System.out.println("ENTER NAME OF EMPLOYEE'S SALARY : ");
+    salary=sc.nextFloat();
+    
+    System.out.println("Employee name = " + name);
+    System.out.println("Employee address = " +address );
+    System.out.println("Employee year of joining = " + yearjoined);
+    System.out.println("Employee salary = " + salary);
+    
+    //EMPLOYEE2 DETAILS
+    System.out.println("ENTER NAME OF EMPLOYEE2 :");
+    name=sc.nextLine();
+    
+    System.out.println("ENTER NAME OF EMPLOYEE'S ADDRESS : ");
+    address=sc.nextLine();
+    
+    System.out.println("ENTER THE YEAR OF JOINING : ");
+    yearjoined=sc.nextInt();
+    
+    System.out.println("ENTER NAME OF EMPLOYEE'S SALARY : ");
+    salary=sc.nextFloat();
+   
+    System.out.println("Employee name2 = " + name);
+    System.out.println("Employee address = " +address );
+    System.out.println("Employee year of joining = " + yearjoined);
+    System.out.println("Employee salary = " + salary);
+    
+    //EMPLOYEE DETAILS3
+     System.out.println("ENTER NAME OF EMPLOYEE2 : ");
+    name=sc.nextLine();
+    
+    System.out.println("ENTER NAME OF EMPLOYEE'S ADDRESS : ");
+    address=sc.nextLine();
+    
+    System.out.println("ENTER THE YEAR OF JOINING : ");
+    yearjoined=sc.nextInt();
+    
+    System.out.println("ENTER NAME OF EMPLOYEE'S SALARY : ");
+    salary=sc.nextFloat();
+   
+    System.out.println("Employee name2 = " + name);
+    System.out.println("Employee address = " +address );
+    System.out.println("Employee year of joining = " + yearjoined);
+    System.out.println("Employee salary = " + salary);
+  
+    
+    
+    
+    
+    
+}
+}
