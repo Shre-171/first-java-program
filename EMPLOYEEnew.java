8d72c936c629b0db293c7dde4310d093fc60c2f6
Shre-171 <85492752+Shre-171@users.noreply.github.com>
2021-06-27 15:18:37 +0530

    Add files via upload
    CLASS AND OBJECTS

---
+++ EMPLOYEEnew.java
@@ -0,0 +1,43 @@
+
+package javaapplication4;
+
+import java.util.Scanner;
+
+
+public class EMPLOYEEnew {
+   public static void main(String[] args){
+       
+    
+    String name,address;
+    int yearjoined;
+    float salary;
+    
+    Scanner sc=new Scanner(System.in);
+    System.out.println("ENTER NAME OF EMPLOYEE: ");
+    name=sc.nextLine();
+    
+    System.out.println("ENTER NAME OF EMPLOYEE'S ADDRESS : ");
+    address=sc.nextLine();
+    
+    System.out.println("ENTER THE YEAR OF JOINING : ");
+    yearjoined=sc.nextInt();
+    
+    System.out.println("ENTER NAME OF EMPLOYEE'S SALARY : ");
+    salary=sc.nextFloat();
+    
+ 
+  
+    {
+        System.out.println("Employee name = " + name);
+    System.out.println("Employee address = " +address );
+    System.out.println("Employee year of joining = " + yearjoined);
+    System.out.println("Employee salary = " + salary);
+  for(int i=0;i<3;i++)
+    {
+        EMPLOYEE e=new EMPLOYEE();
+        System.out.println(e);
+    }
+    
+}
+}
+}
